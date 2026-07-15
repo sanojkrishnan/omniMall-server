3f81196a5596621d82c261ecb52119da044bafb2
sanoj krishnan m <sanojkrishnan12@gmail.com>
2026-07-15 22:37:46 +0530

    date 15/7/2026

--- models/Cart.js
+++ models/Cart.js
@@ -2,24 +2,25 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema(
   {
-    useId: {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: [true, "User not found"],
     },
-    cart: {
-      sellerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, "Seller not found"],
+    cart: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: [true, "Product not found"],
+        },
+        sellerId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Seller",
+          required: [true, "Seller not found"],
+        },
+        qnty: { type: Number, required: [true, "How much quantity"] },
       },
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, "Product not found"],
-      },
-      qnty: {
-        type: Number,
-        required: [true, "How much quantity"],
-      },
-    },
+    ],
   },
   { timestamps: true },
 );
